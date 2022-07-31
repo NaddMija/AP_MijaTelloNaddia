9f03cd7db3c574f442eb90de61c533ce6314e2c0
NaddMija <mijanaddia@gmail.com>
2022-07-31 02:45:11 -0300

    SE HIZO UN NUEVO SKILLS

--- Backend/src/main/java/com/apMija/nmt/Repository/RSkills.java
+++ Backend/src/main/java/com/apMija/nmt/Repository/RSkills.java
@@ -10,7 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RSkills extends JpaRepository<Skills, Integer>{
-    public Optional<Skills> findByNombreS(String nombreS);
-    public boolean existsByNombreS(String nombreS);  
+
+public interface RSkills extends JpaRepository<Skills, Integer> {
+    public Optional <Skills> findByNombreS(String nombreS);
+    public boolean existsByNombreS(String nombreS);
+    
 }
